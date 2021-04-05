5e29fb50a1b21ae964311e4810b6e62bc76646d5
ritesh845 <riteshpanchal845@gmail.com>
2021-04-05 18:16:40 +0530

    test

---
+++ resources/views/template/partials/slider.blade.php
@@ -0,0 +1,51 @@
+{{-- <div class="hero-slider position-relative slick-initialized slick-slider slick-dotted">
+  <button type="button" class="prevArrow slick-arrow" style=""><i class="ti-arrow-left"></i></button>
+
+    <div class="slick-list draggable"><div class="slick-track" style="opacity: 1; width: 11538px; transform: translate3d(-5128px, 0px, 0px);">
+      <div class="hero-slider-item py-160 slick-slide slick-cloned" style="background-image: url('http://127.0.0.1:8000/slider.jpg'); width: 1282px;" data-icon="ti-package" data-text="Human Resources" data-slick-index="-1" aria-hidden="true" tabindex="-1">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="hero-content">
+              <h4 class="text-uppercase mb-1" data-duration-in=".5" data-animation-in="fadeInDown" data-delay-in=".1" style="opacity: 0;">We
+                are always</h4>
+              <h1 class="font-weight-bold mb-3" data-duration-in=".5" data-animation-in="fadeInDown" data-delay-in=".5" style="opacity: 0;">
+                Be Inspired By Best</h1>
+              <p class="text-dark mb-50" data-duration-in=".5" data-animation-in="fadeInDown" data-delay-in=".9" style="opacity: 0;">Lorem
+                ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                <br> incididunt ut labore et dolore magna aliqua.
+              </p>
+              <a data-duration-in=".5" data-animation-in="fadeInDown" data-delay-in="1.3" href="about-2.html" class="btn btn-outline text-uppercase" tabindex="-1" style="opacity: 0;">more details</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+    
+    
+    
+  <button type="button" class="nextArrow slick-arrow" style=""><i class="ti-arrow-right"></i></button>
+</div>
+ --}}
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" height="400" src="{{asset('slider.jpg')}}" alt="First slide">
+       <div class="carousel-caption d-none d-md-block">
+        <h5 class="font-weight-bold text-white">PROFESSION DEDICATED EMAIL SERVICE</h5>
+        <p  class="font-weight-bold text-white">Dress & Address
+Makes The Difference</p>
+      </div>
+    </div>
+    
+    {{-- <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide">
+    </div> --}}
+  </div>
+ 
+</div>
