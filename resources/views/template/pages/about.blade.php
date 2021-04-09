d59393d686599a4b6df299b5b980b294e902a4be
ritesh845 <riteshpanchal845@gmail.com>
2021-04-09 17:01:36 +0530

    about page

--- resources/views/template/pages/about.blade.php
+++ resources/views/template/pages/about.blade.php
@@ -3,7 +3,7 @@
         <div class="row">
                 <!-- Blog Post -->
            	@foreach($posts as $post)
-                <div class="col-lg-4 col-sm-6 mb-5 mb-lg-0">
+                <div class="col-lg-4 col-sm-6 ">
                     <div class="card text-center">
                       <h4 class="card-title pt-3" style="height:60px !important">{{$post->title}}</h4>
                       <div class="card-img-wrapper">
@@ -17,42 +17,7 @@
                       </div>
                     </div>
                 </div>
-
-
-               
-
              @endforeach           
         </div>
     </div>
 </section>
-
-<!-- <article class="bg-white rounded mb-40">
-                        <a href="blog-single.html">
-                            <img class="img-fluid w-100 rounded-top" src="{{asset($post->image_path !=null ? 'storage/'.$post->image_path : 'no_image.jpg')}}" alt="{{$post->title}}">
-                        </a>
-                    <div>
-                        <div class="d-flex align-items-center border-bottom">
-                            <div class="text-center border-right p-4">
-                                <h3 class="text-primary mb-0">
-                                    {{date('d',strtotime($post->start_date))}}
-                                    <span class="d-block paragraph">{{date('M',strtotime($post->start_date))}}</span>
-                                    <span class="d-block paragraph">{{date('Y',strtotime($post->start_date))}}</span>
-                                </h3>
-                            </div>
-                            <div class="px-4">
-                                <a class="h4 d-block mb-10" href="blog-single.html">{{$post->title}}</a>
-                                <ul class="list-inline">
-                                    <li class="list-inline-item paragraph mr-5">By
-                                        <a href="#" class="paragraph">{{$post->user->name}}</a>
-                                    </li>
-                                    <li class="list-inline-item paragraph">{{$post->category->catg_name}}</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="p-4">
-                            {{-- {!! Str::limit($post->body,200,$end='...') !!} --}}
-                            
-                            <a href="{{url('/'.(strtolower($post->category->catg_name)).'/'.$post->sefriendly)}}" class="btn btn-sm btn-primary">Read More</a>
-                        </div>
-                    </div>
-                </article> -->
