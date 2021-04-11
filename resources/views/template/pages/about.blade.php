7a2ca573a66b744c65df443abbf93c9957c0fdb2
ritesh845 <riteshpanchal845@gmail.com>
2021-04-11 09:48:01 +0530

    home page changes

--- resources/views/template/pages/about.blade.php
+++ resources/views/template/pages/about.blade.php
@@ -1,4 +1,4 @@
-<section class="bg-gray">
+{{-- <section class="bg-gray">
     <div class="container p-5">
         <div class="row">
            	@foreach($posts as $post)
@@ -28,7 +28,7 @@
     });
 </script>
 </section>
-
+ --}}
 
 {{-- <h4 class="card-title pt-3" style="height:60px !important">{{$post->title}}</h4>
 <div class="card-img-wrapper">
@@ -40,3 +40,55 @@
 <a href="{{url('/'.(strtolower($post->category->catg_name)).'/'.$post->sefriendly)}}" class="btn btn-secondary translateY-25">Read
   More</a>
 </div> --}}
+
+
+<section class="bg-gray">
+    <div class="container">
+        <div class="row">
+            @foreach($posts as $post)
+            <div class="col-lg-4 py-100">
+                <!-- Blog Post -->
+                <article class="bg-white rounded mb-40">
+                    <!-- Post Thumbnail -->
+                        <a href="blog-single.html">
+                            <img class="img-fluid w-100 rounded-top" src="{{asset($post->image_path !=null ? 'storage/'.$post->image_path : 'no_image.jpg')}}" alt="{{$post->title}}">
+                        </a>
+                    <!-- Post Content -->
+                    <div>
+                        <div class="d-flex align-items-center border-bottom">
+                            <!-- Published Date -->
+                            <div class="text-center border-right p-4">
+                                <h3 class="text-primary mb-0">
+                                    {{date('d',strtotime($post->start_date))}}
+                                    <span class="d-block paragraph">{{date('M',strtotime($post->start_date))}}</span>
+                                    <span class="d-block paragraph">{{date('Y',strtotime($post->start_date))}}</span>
+                                </h3>
+                            </div>
+                            <div class="px-4">
+                                <!-- Post Title -->
+                                <a class="h4 d-block mb-10" href="blog-single.html">{{$post->title}}</a>
+                                <!-- Post Meta -->
+                                <ul class="list-inline">
+                                    <li class="list-inline-item paragraph mr-5">By
+                                        <a href="#" class="paragraph">{{$post->user->name}}</a>
+                                    </li>
+                                    <li class="list-inline-item paragraph">{{$post->category->catg_name}}</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <!-- Post Excerpts -->
+                        <div class="p-4">
+                            {{-- {!! Str::limit($post->body,200,$end='...') !!} --}}
+                            
+                            <a href="{{url('/'.(strtolower($post->category->catg_name)).'/'.$post->sefriendly)}}" class="btn btn-sm btn-primary">Read More</a>
+                        </div>
+                    </div>
+                </article>
+
+           
+            </div>
+             @endforeach
+           
+        </div>
+    </div>
+</section>
