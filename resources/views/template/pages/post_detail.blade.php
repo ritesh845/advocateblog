b2e91156e3e12e257b7dafbaca60b5609fc9ca82
ritesh845 <riteshpanchal845@gmail.com>
2021-04-11 10:07:54 +0530

    post common page style change

--- resources/views/template/pages/post_detail.blade.php
+++ resources/views/template/pages/post_detail.blade.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-lg-8 py-100">
                 <div class="border rounded bg-white">
-                    <img class="img-fluid w-100 rounded-top" src="{{asset($post->image_path !=null ? 'storage/'.$post->image_path : 'no_image.jpg')}}" alt="{{$post->title}}">
+                    <img class="img-fluid w-100 rounded-top" src="{{asset($post->image_path !=null ? 'storage/'.$post->image_path : 'no_image.jpg')}}" alt="{{$post->title}}" style="min-height: 250px; max-height: 250px">
                     <div class="p-4">
                         <h3>{{$post->title}}</h3>
                         <ul class="list-inline d-block pb-4 border-bottom mb-3">
@@ -48,47 +48,7 @@
                     </div>
                 </div> --}}
             </div>
-            <div class="col-lg-4">
-                <!-- Sidebar -->
-                <div class="bg-white px-4 py-100 sidebar-box-shadow">
-                    <!-- Search Widget -->
-                    {{-- <div class="mb-50">
-                        <h4 class="mb-3">Search Here</h4>
-                        <div class="search-wrapper">
-                            <input type="text" class="form-control" name="search" placeholder="Type Here...">
-                        </div>
-                    </div> --}}
-                    <!-- categories -->
-                    <div class="mb-50">
-                        <h4 class="mb-3">Categories</h4>
-                        <ul class="pl-0 mb-0">
-                            <li class="border-bottom">
-                                <a href="#" class="d-block text-color py-10">Business Analysis</a>
-                            </li>
-                           
-                        </ul>
-                    </div>
-                    <!-- Widget Recent Post -->
-                    <div class="mb-50">
-                        <h4 class="mb-3">Recent News</h4>
-                        <div class="d-flex py-3 border-bottom">
-                            <div class="mr-4">
-                                <a href="blog-single.html">
-                                    <img class="rounded" src="images/blog/post-thumb-sm-01.jpg" alt="post-thumb">
-                                </a>
-                            </div>
-                            <div>
-                                <h6 class="mb-3">
-                                    <a class="text-dark" href="blog-single.html">Marketing Strategy 2017-2018.</a>
-                                </h6>
-                                <p class="meta">16 Dec, 2018</p>
-                            </div>
-                        </div>
-                       
-                    </div>
-                    <!-- Widget Tags -->
-                </div>
-            </div>
+            @include('template.pages.post-sidebar')
         </div>
     </div>
 </section>
